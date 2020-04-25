5243bd8abe8d5da71a3ec1db9e73f4585ac059cc
amichaelm <zmarti90@gmail.com>
2020-04-25 15:01:43 -0500

    add to top bow n arrow svg to post page.

--- single.php
+++ single.php
@@ -1,15 +1,13 @@
 <?php get_header();?>
 
-
-<div class="single" style="background-image: url('<?php bloginfo("template_directory");?>/images/blogBack.svg')">
+<div class="single" id="single" style="background-image: url('<?php bloginfo("template_directory");?>/images/blogBack.svg')">
 
    <h1 class="single__title heading-1"><?php the_title();?></h1>
    
 
    <?php if(has_post_thumbnail()):?>
-
+   
       <!-- <img src="<?php the_post_thumbnail_url('largest');?>" class="single__img"> -->
-
 
    <?php endif;?>
 
@@ -22,6 +20,10 @@
    <?php endwhile; endif;?>
 
 </div>
+
+   <a href="#single">
+      <img src="<?php bloginfo('template_directory');?>/images/bownarrow.svg" class="archive__bownarrow" alt="a bow n arrow pointing up toward the top of page.">
+   </a>
 
 </div>
 
