aa954b40d81f8534256c2a965a827c861e3ffe79
amichaelm <zmarti90@gmail.com>
2020-04-25 16:06:47 -0500

    add copyright and website info in footer.

--- footer.php
+++ footer.php
@@ -3,42 +3,51 @@
 <footer class="footer">
 <?php wp_footer();?> 
 
-<div class="footer__navigation">
-    <h3 class="">
-        Navigation
-    </h3>
-    <ul class="footer__navigation--list">
-        <li><a href="<?php bloginfo('url');?>">home</a></li>
-        <li><a href="http://www.sidetracked.blog/category/archive">archive</a></li>
-        <li><a href="#">about</a></li>
-    </ul>
-</div>
+    <div class="footer__navigation">
+        <h3 class="">
+            Navigation
+        </h3>
+        <ul class="footer__navigation--list">
+            <li><a href="<?php bloginfo('url');?>">home</a></li>
+            <li><a href="http://www.sidetracked.blog/category/archive">archive</a></li>
+            <li><a href="#">about</a></li>
+        </ul>
+    </div>
 
-<div class="footer__follow">
-    <h3 class="">
-        Follow Us
-    </h3>
-    <ul class="footer__navigation--list">
-        <li><a href="https://www.instagram.com/donniestephens/"><img src="<?php bloginfo('template_directory');?>/images/insta.svg" class="instaSvg">instagram</a></li>
-    </ul>
-</div>
+    <div class="footer__follow">
+        <h3 class="">
+            Follow Us
+        </h3>
+        <ul class="footer__navigation--list">
+            <li><a href="https://www.instagram.com/donniestephens/"><img src="<?php bloginfo('template_directory');?>/images/insta.svg" class="instaSvg">instagram</a></li>
+        </ul>
+    </div>
 
-<div class="footer__ask">
-    <h3 class="">
-        Ask Us
-    </h3>
-    <ul class="footer__navigation--list">
-        <li>
-            <a href="mailto:stephens.dona@gmail.com">
-                <img src="<?php bloginfo('template_directory');?>/images/gmail.svg" class="gmailSvg">
-                email
-            </a>
-        </li>
-    </ul>
-</div>
-
+    <div class="footer__ask">
+        <h3 class="">
+            Ask Us
+        </h3>
+        <ul class="footer__navigation--list">
+            <li>
+                <a href="mailto:stephens.dona@gmail.com">
+                    <img src="<?php bloginfo('template_directory');?>/images/gmail.svg" class="gmailSvg">
+                    email
+                </a>
+            </li>
+        </ul>
+    </div>
 </footer>
 
+    <div class="info">
+        <p class="info__copyright">©2020 sidetracked blog. All Rights Reserved</p>
+        
+        <p class="info__by">
+            Website made on 🌎 
+            <a href="amichaelm.github.io">
+                byalexm.com
+            </a>
+        </p>
+    </div>
 
 
 
